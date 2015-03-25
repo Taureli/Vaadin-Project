eaf0fc27ec0337d07f88c1fd26b84e2a7b4938f8
Taureli <hobkinss@gmail.com>
2015-03-25 18:21:18 +0100

    Game layout prep

--- src/main/java/com/jakub/vaadinprojekt/MyUI.java
+++ src/main/java/com/jakub/vaadinprojekt/MyUI.java
@@ -1,5 +1,8 @@
 package com.jakub.vaadinprojekt;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.servlet.annotation.WebServlet;
 
 import com.jakub.vaadinprojekt.domain.Person;
@@ -27,32 +30,24 @@ public class MyUI extends UI {
 
 	Person person = new Person();
 	BeanFieldGroup<Person> item = new BeanFieldGroup<Person>(Person.class);
+	
+	//Board buttons
+	private List<Button> buttons = new ArrayList<Button>();
 
 	//Layouts
     final VerticalLayout loginLayout = new VerticalLayout();
     final VerticalLayout lobbyLayout = new VerticalLayout();
+    final VerticalLayout gameLayout = new VerticalLayout();
     
 	@Override
     protected void init(VaadinRequest vaadinRequest) {
 		getPage().setTitle("Tic Tac Toe Online!");
-
-		loginLayout.setMargin(true);
-        lobbyLayout.setMargin(true);
-        
         item.setItemDataSource(person);
-        loginLayout.addComponent(loginForm());        
         
-        //First button
-        Button testBtn = new Button("Wyloguj");
-        testBtn.addClickListener(new Button.ClickListener() {
-			private static final long serialVersionUID = 1L;
-
-			@Override
-            public void buttonClick(ClickEvent event) {
-                setContent(loginLayout);
-            }
-        });
-        lobbyLayout.addComponent(testBtn);
+		loginLayout.setMargin(true);
+        prepareLobbyLayout();
+        prepareGameLayout();
+        loginLayout.addComponent(loginForm());        
 
         //Setting content - login screen if not logged, else - lobby screen
         if(VaadinSession.getCurrent().getSession().getAttribute("user") == null)
@@ -63,10 +58,17 @@ public class MyUI extends UI {
         }
     }
 	
-	//creating login button
-	public Button loginButton(){
-		Button button = new Button("Zaloguj");
-		button.addClickListener(new Button.ClickListener() {
+	//-------------LOGIN-FORM----------------
+	public FormLayout loginForm(){
+		FormLayout form = new FormLayout();
+		
+		AbstractTextField name = (AbstractTextField) item.buildAndBind("Nickname", "nickname");
+		name.setNullRepresentation("");
+		form.addComponent(name);
+		
+		//Creating login button
+		Button loginButton = new Button("Zaloguj");
+		loginButton.addClickListener(new Button.ClickListener() {
 			private static final long serialVersionUID = 1L;
 			@Override
 			public void buttonClick(ClickEvent event) {
@@ -83,21 +85,63 @@ public class MyUI extends UI {
 				}
 			}
 		});
-		return button;
-	}
-	
-	//login form
-	public FormLayout loginForm(){
-		FormLayout form = new FormLayout();
+		form.addComponent(loginButton);
 		
-		AbstractTextField name = (AbstractTextField) item.buildAndBind("Nickname", "nickname");
-		name.setNullRepresentation("");
-		form.addComponent(name);
-		
-		form.addComponent(loginButton());
 		return form;
 	}
+	//-----------------------------------------
+	
+	//-----------LOBBY-LAYOUT-------------
+	void prepareLobbyLayout(){
+        lobbyLayout.setMargin(true);
 
+        //Logout button
+        Button gameBtn = new Button("Graj");
+        gameBtn.addClickListener(new Button.ClickListener() {
+			private static final long serialVersionUID = 1L;
+
+			@Override
+            public void buttonClick(ClickEvent event) {
+                setContent(gameLayout);
+            }
+        });
+        
+      //Logout button
+        Button logoutBtn = new Button("Wyloguj");
+        logoutBtn.addClickListener(new Button.ClickListener() {
+			private static final long serialVersionUID = 1L;
+
+			@Override
+            public void buttonClick(ClickEvent event) {
+				//TODO: ACTUAL LOGOUT FROM SESSION
+                setContent(loginLayout);
+            }
+        });
+        
+        lobbyLayout.addComponent(gameBtn);
+        lobbyLayout.addComponent(logoutBtn);
+	}
+	//------------------------------------
+	
+	//----------GAME-LAYOUT----------
+	void prepareGameLayout(){
+        gameLayout.setMargin(true);
+        
+        //create board
+		for(int i = 0; i < 9; i++){
+			buttons.add(new Button(""));
+			buttons.get(i).addClickListener(new Button.ClickListener() {
+				private static final long serialVersionUID = 1L;
+				@Override
+				public void buttonClick(ClickEvent event) {
+					//AKCJA
+				}
+			});
+			gameLayout.addComponent(buttons.get(i));
+		}
+	}
+	//--------------------------------
+	
     @WebServlet(urlPatterns = "/*", name = "MyUIServlet", asyncSupported = true)
     @VaadinServletConfiguration(ui = MyUI.class, productionMode = false)
     public static class MyUIServlet extends VaadinServlet {
